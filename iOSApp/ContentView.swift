b18c8f13b2cfe5657f4993e17698bcef57018cb4
Dmytro Ipatii <dmyto.ipatii@aalto.it>
2026-03-03 15:07:42 +0100

    Initial Commit

---
+++ iOSApp/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  iOSApp
+//
+//  Created by Dmytro Ipatii on 06/02/26.
+//
+
+import SwiftUI
+import KMPSharedLibrary
+
+struct ContentView: View {
+    @State var greetPlatform: String = "Hello! This is the KMP Library designed for ${platform} v.${version}"
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text(greetPlatform)
+        }
+        .padding()
+        .onAppear {
+            let platformInfo = PlatformInfo()
+            let platformName = platformInfo.getPlatformName()
+            let platformVersion = platformInfo.getPlatformVersion()
+            
+            
+            greetPlatform = greetPlatform.replacingOccurrences(of: "${platform}", with: platformName)
+            greetPlatform = greetPlatform.replacingOccurrences(of: "${version}", with: "\(platformVersion)")
+            
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by Dmytro Ipatii on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
